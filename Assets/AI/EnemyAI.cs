3a90657951883da44a2e7859aaf2895d05a3ff09
Pranav Balu <51266456+pranavbalu1@users.noreply.github.com>
2024-08-27 22:38:37 -0500

    add patrol behaviour

--- Assets/AI/EnemyAI.cs
+++ Assets/AI/EnemyAI.cs
@@ -11,67 +11,58 @@ public class EnemyAI : MonoBehaviour
 
     public LayerMask whatIsGround, whatIsPlayer;
 
-    public float health;
+    public float health = 10;  // Set a default health value
 
-    //Patroling
-    public Vector3 walkPoint;
-    bool walkPointSet = false;
-    public float walkPointRange;
 
     //States
-    public float sightRange;
-    public bool playerInSightRange;
+    public float sightRange = 20;
+    public bool playerInSightRange = false;
+    private float enemySpeed = 5;
 
     private void Awake()
     {
         player = GameObject.Find("Player").transform;
         agent = GetComponent<NavMeshAgent>();
     }
+
     private void Update()
     {
         //Check for sight and attack range
         playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
 
-        if (!playerInSightRange) Patroling();
-        if (playerInSightRange) ChasePlayer();
-
+        if (!playerInSightRange)
+            Patroling();
+        else
+            ChasePlayer();
     }
+
     private void Patroling()
     {
-        if (!walkPointSet) SearchWalkPoint();
-
-        if (walkPointSet)
-            agent.SetDestination(walkPoint);
-
-        Vector3 distanceToWalkPoint = transform.position - walkPoint;
-
-        //Walkpoint reached
-        if (distanceToWalkPoint.magnitude < 1f)
-            walkPointSet = false;
-        Debug.Log("Patroling");
-    }
-    private void SearchWalkPoint()
-    {
-        //Calculate random point in range
-        float randomZ = Random.Range(-walkPointRange, walkPointRange);
-        float randomX = Random.Range(-walkPointRange, walkPointRange);
-
-        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
-
-        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+        if (agent.remainingDistance < 0.5f)
         {
-            walkPointSet = true;
-            Debug.Log("Searching Walk Point");
+            float x = Random.Range(-11, 11);
+            float z = Random.Range(-11, 11);
+            Vector3 position = new Vector3(x, 0f, z);
+            agent.SetDestination(position);
+            //Debug.Log($"Patrolling to {patrolPoints[currentPatrolPointIndex].position}");
         }
-            
     }
 
     private void ChasePlayer()
     {
-        agent.SetDestination(player.position);
-        Debug.Log("Chasing Player");
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        if (distanceToPlayer > 4f)
+        {
+            agent.SetDestination(player.position);
+            agent.speed = enemySpeed;
+            Debug.Log($"Chasing Player {player.position}");
+        }
+        else
+        {
+            agent.isStopped = true;
+            Debug.Log($"Chasing Player {player.position}. Slowing down.");
+        }
     }
-
-
 
 }
