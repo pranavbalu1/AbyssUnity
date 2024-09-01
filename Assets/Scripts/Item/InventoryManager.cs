eb1567892236fa7b1b8645771ed417c814ce7b14
Pranav Balu <51266456+pranavbalu1@users.noreply.github.com>
2024-09-01 17:45:43 -0500

    implement scriptable object skeleton
    TODO:
    - scrolling through the hotbar
    - using items

--- Assets/Scripts/Item/InventoryManager.cs
+++ Assets/Scripts/Item/InventoryManager.cs
@@ -1,22 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryManager : MonoBehaviour
 {
+    [SerializeField] private GameObject slotHolder;
+    
     public List<ItemClass> inventory = new List<ItemClass>();
+    
+    private GameObject[] slots;
 
     public void Start()
     {
-       
+       slots = new GameObject[slotHolder.transform.childCount];
+        for (int i = 0; i < slotHolder.transform.childCount; i++)
+        {
+            slots[i] = slotHolder.transform.GetChild(i).gameObject;
+        }
+
+        RefreshUI();
+    }
+
+    public void RefreshUI()
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            try 
+            { 
+            slots[i].transform.GetChild(0).GetComponent<Image>().sprite = inventory[i].item_icon;
+            }
+            catch
+            {
+                slots[i].transform.GetChild(0).GetComponent<Image>().sprite = null;
+                slots[i].transform.GetChild(0).GetComponent<Image>().enabled = false;
+            }
+        }
     }
 
     public void AddItem(ItemClass item)
     {
         inventory.Add(item);
+        RefreshUI();
     }
     public void RemoveItem(ItemClass item) 
     {
         inventory.Remove(item);
+        RefreshUI();
+
     }
 }
