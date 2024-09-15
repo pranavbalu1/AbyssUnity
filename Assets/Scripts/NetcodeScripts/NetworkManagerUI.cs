aa123c967b7a4937f8068998203921bf7a00ecf1
Pranav Balu <51266456+pranavbalu1@users.noreply.github.com>
2024-09-15 16:08:01 -0500

    add network ui

---
+++ Assets/Scripts/NetcodeScripts/NetworkManagerUI.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Unity.Netcode;
+
+public class NetworkManagerUI : MonoBehaviour
+{
+    [SerializeField] private Button serverBtn;
+    [SerializeField] private Button hostBtn;
+    [SerializeField] private Button clientBtn;
+
+    private void Awake()
+    {
+        serverBtn.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartServer();
+        });
+        hostBtn.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartHost();
+        });
+        clientBtn.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartClient();
+        });
+    }
+}
